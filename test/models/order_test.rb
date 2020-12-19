71abfafe9e6ef16e1cf3ffd4d4f8c0ad09f2502f
Matías <matias.mullerg@mail.udp.cl>
2020-12-19 14:22:06 -0300

    first commit

---
+++ test/models/order_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+
+class OrderTest < ActiveSupport::TestCase
+  test "generates a random number on create" do
+    user = users(:one)
+    order = Order.create(user_id: user.id)
+
+    assert_not order.number.nil?
+  end
+
+  test 'number must be unique' do
+    user = users(:one)
+    order = Order.create(user_id: user.id)
+    dup_order = order.dup
+
+    assert_not dup_order.valid?
+  end
+
+  test 'adds products as order_items' do
+    user = users(:one)
+    order = Order.create(user_id: user.id)
+    product = products(:one)
+    quantity = 1
+    order.add_product(product.id, quantity)
+
+    assert_equal(order.order_items.count, 1)
+  end
+
+  test 'products with zero stock cant be added to cart' do
+    user = users(:one)
+    order = Order.create(user_id: user.id)
+    product = products(:two)
+    quantity = 1
+    order.add_product(product.id, quantity)
+
+    assert_equal(order.order_items.count, 0) 
+  end
+end
